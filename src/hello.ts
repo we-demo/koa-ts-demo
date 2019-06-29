f75ea3ffbd05e514041f693e28f79f276bb93d67
Fritz Lin <uxfritz@163.com>
2019-06-29 18:37:02 +0800

    add @types/node & fs call

--- src/hello.ts
+++ src/hello.ts
@@ -1,8 +1,14 @@
+import { promises as fs } from 'fs'
+
 type Hello = string
 
-function hello(str: Hello) {
+async function hello(str: Hello) {
   // tslint:disable-next-line no-console
   console.log(['hello', str])
+
+  let files = await fs.readdir('.')
+  // tslint:disable-next-line no-console
+  console.log(['files', files.length])
 }
 
 hello('world')
